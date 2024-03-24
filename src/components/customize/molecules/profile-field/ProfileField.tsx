d9632d2a8cfa6dbd6e1b41a2cc313e038dbf0f4b
Ray Hans Sebastian Mantiri <rayhansmantiri@gmail.com>
2024-03-24 22:23:11 +0700

    feat: add edit icons on profile

--- src/components/customize/molecules/profile-field/ProfileField.tsx
+++ src/components/customize/molecules/profile-field/ProfileField.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Pencil } from "lucide-react";
 import React, { useState } from "react";
 
 function ProfileField() {
@@ -11,18 +12,21 @@ function ProfileField() {
 
   return (
     <div
-      className="w-20 h-20 rounded-full bg-cover bg-no-repeat bg-center hover:brightness-50"
+      className="relative w-20 h-20 rounded-full bg-cover bg-no-repeat bg-center hover:brightness-50 transition-all"
       style={{
         backgroundImage: profile
           ? `url(${profile})`
           : 'url("/assets/image-placeholder.png")',
       }}
     >
+      <div className="absolute w-8 h-8 rounded-full p-2 -bottom-1 -right-1 bg-teal-500">
+        <Pencil color="white" size={16} />
+      </div>
       <input
         type="file"
         name=""
         id=""
-        className="opacity-0 w-full h-full cursor-pointer"
+        className="opacity-0 w-full h-full rounded-full cursor-pointer"
         onChange={handleUploadProfile}
       />
     </div>
