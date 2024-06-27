b810d9a8ef3379b3413e25ac5e57f20ada84d2cc
Ray Hans Sebastian Mantiri <rayhansmantiri@gmail.com>
2024-06-27 21:02:44 +0700

    fix: register

--- src/components/customize/atoms/Checkbox.tsx
+++ src/components/customize/atoms/Checkbox.tsx
@@ -5,20 +5,26 @@ type IProps = {
   description: string;
   checked: boolean;
   onChange: (checked: boolean) => void;
+  disabled?: boolean;
 };
 
-function Checkbox({ label, description, checked, onChange }: IProps) {
+function Checkbox({ label, description, checked, onChange, disabled }: IProps) {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onChange(event.target.checked);
   };
 
   return (
-    <div className="flex items-center gap-4 border rounded-lg p-4 w-full">
+    <div
+      className={`${
+        disabled ? "bg-slate-200" : ""
+      } flex items-center gap-4 border rounded-lg p-4 w-full`}
+    >
       <input
         className="accent-teal-700 w-4 h-4"
         type="checkbox"
         checked={checked}
         onChange={handleChange}
+        disabled={disabled}
       />
       <div>
         <p className="text-sm font-semibold lining-nums">{label}</p>
